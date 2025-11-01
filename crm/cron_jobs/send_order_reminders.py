b0d20b0aa728cbbd9f23f70db1788e01af85a09f
Ahmed <abasimelahmed@gmail.com>
2025-11-01 22:52:43 +0100

    Added send_order_reminders.py

---
+++ crm/cron_jobs/send_order_reminders.py
@@ -0,0 +1,51 @@
+
+import os
+from gql import gql,Client
+from gql.transport.requests import RequestsHTTPTransport
+from datetime import datetime,timedelta
+
+LOG_FILE="/tmp/order_reminders_log.txt"
+GRAPHQL_ENDPOINT=  "http://localhost:8000/graphql"
+
+
+def send_order_reminders():
+    one_week_ago=(datetime.now()-timedelta(days=7)).isoformat()
+
+    query=gql("""
+            query($since:DateTime!){
+              allOrders(orderBy:"order_date", filter:{status:"Pending",orderDate_Gte:$since}){
+                edges{
+                    node{
+                        id
+                        customer{
+                        email
+                        }
+                        orderDate
+              
+                            }
+              
+              
+                        }
+              
+                 }
+              
+              
+              }
+""")
+    
+    transport=RequestsHTTPTransport(url=GRAPHQL_ENDPOINT,verify=True,retries=3)
+    client=Client(transport=transport,fetch_schema_from_transport=True)
+
+    result=client.execute(query,variable_values={"since": one_week_ago})
+    orders=result.get("allOrders",{}).get("edges",[])
+    with open(LOG_FILE, 'a') as f:
+        for order_edge in orders:
+            order=order_edge["node"]
+            log_line=f"{datetime.now().isoformat()}-Oder ID: {order[id]}, Customer: {order['customer']['email']}, Order Date: {order['orderDate']}\n"
+            f.write(log_line)
+    print("Order reminders processed!")
+
+
+
+if __name__=="__main__":
+    send_order_reminders()
