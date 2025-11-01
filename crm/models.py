05c9495e243fd74395d804ac9b6024deb5b7f341
Ahmed <abasimelahmed@gmail.com>
2025-11-01 12:44:40 +0100

    Added OrderProduct for manay to many relationship

--- crm/models.py
+++ crm/models.py
@@ -25,15 +25,33 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
-    products = models.ManyToManyField(Product, related_name='orders')
+    products = models.ManyToManyField(Product, through='OrderProduct',related_name='orders')
     order_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+    )
 
-    def save(self, *args, **kwargs):
-        # Calculate total_amount automatically
-        self.total_amount = sum([p.price for p in self.products.all()])
-        super().save(*args, **kwargs)
+    def calculate_total_amount(self):
+        total=sum(op.product.price*op.quantity for op in self.order_products.all())
+        self.total_amount=total
+        self.save(update_fields=['total_amount'])
+        
 
     def __str__(self):
         return f"Order {self.id} - {self.customer.name}"
+
+class OrderProduct(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='order_products')
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity} for {self.order.id}"
